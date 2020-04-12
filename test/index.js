3fc273a9dad133a04ee878776bd8b77da3e61082
chris <chris.saretzky@gmail.com>
2020-04-11 20:55:00 -0700

    Added unit testing cases

--- test/index.js
+++ test/index.js
@@ -95,29 +95,83 @@ describe('Filterer', function(){
 
   describe('#add_filter(name, fn(data, criteria), criteria)', function() {
     context('when name is null or not a string', function(){
-      it('should throw an error', function(){})
+      it('should throw an error', function(){
+        expect(filterer.add_filter(
+          '',
+          function(data, criteria){return (data === criteria.name) ? true : false},
+          criteria: {name: 'test1'}
+        )).to.throw(
+          TypeError,
+          'Invalid filter name'
+        )
+      })
     })
     context('when fn argument is not a function', function(){
-      it('should throw an error', function(){})
+      it('should throw an error', function(){
+        expect(filterer.add_filter(
+          'test_filter',
+          null,
+          criteria: {name: 'test1'}
+        )).to.throw(
+          TypeError,
+          'Invalid function'
+        )
+      })
     })
     context('when criteria argument is null', function(){
-      it('should throw an error', function(){})
+      it('should throw an error', function(){
+        expect(filterer.add_filter(
+          'test_filter',
+          function(data, criteria){return (data === criteria.name) ? true : false},
+          criteria: null
+        )).to.throw(
+          TypeError,
+          'Invalid criteria'
+        )
+      })
     })
     context('when fn argument does not return a boolean', function(){
-      it('should throw an error', function(){})
+      it('should throw an error', function(){
+        expect(filterer.add_filter(
+          'test_filter',
+          function(data, criteria){return (data === criteria.name) ? 1 : 0},
+          criteria: {name: 'test1'}
+        )).to.throw(
+          TypeError,
+          'Submitted function must return a boolean value'
+        )
+      })
     })
     context('when fn argument has 0 or 1 arguments', function(){
-      it('should throw an error', function(){})
+      it('should throw an error', function(){
+        expect(filterer.add_filter(
+          'test_filter',
+          function(criteria){return (criteria.name) ? true : false},
+          criteria: {name: 'test1'}
+        )).to.throw(
+          TypeError,
+          'Submitted function must include 2 arguments'
+        )
+      })
     })
     context('when fn argument and data object are valid', function(){
-      it('should return true', function(){})
+      it('should return true', function(){
+        expect(filterer.add_filter(
+          'test_filter',
+          function(data, criteria){return (data === criteria.name) ? true : false},
+          criteria: {name: 'test1'}
+        )).to.be.true
+      })
     })
   })
 
   describe('#apply_filter(name)', function() {
     context('when filter name is null', function(){
       it('should throw error', function(){
-        filterer.filters.test_filter = {fn: function(){}, criteria: 'test'}
+        filterer.filters.test_filter = {
+          fn: function(data, criteria){return (data === criteria.name) ? true : false},
+          criteria: {name: 'test1'}
+        }
         expect(filterer.apply_filter('')).to.throw(
           TypeError,
           'Invalid filter name'
@@ -127,9 +181,12 @@ describe('Filterer', function(){
     context('when filter name is not a key in the filter object', function(){
       it('should throw error', function(){
         filterer.complete_data = ['test1', 'test2']
-        filterer.filters.test_filter = {fn: function(){}, criteria: 'test'}
+        filterer.filters.test_filter = {
+          fn: function(data, criteria){return (data === criteria.name) ? true : false},
+          criteria: {name: 'test1'}
+        }
         expect(filterer.apply_filter('filter_test')).to.throw(
-          TypeError,
+          Error,
           'Invalid filter name'
         )
       })
@@ -137,46 +194,153 @@ describe('Filterer', function(){
     context('when the complete_data array is empty', function(){
       it('should throw error', function(){
         filterer.complete_data = []
-        filterer.filters.test_filter = {fn: function(){}, criteria: 'test'}
+        filterer.filters.test_filter = {
+          fn: function(data, criteria){return (data === criteria.name) ? true : false},
+          criteria: {name: 'test1'}
+        }
         expect(filterer.apply_filter('test_filter')).to.throw(
-          TypeError,
+          Error,
           'complete_data array empty, provide data to be filtered'
         )
       })
     })
     context('when criteria argument is null', function(){
-      it('should throw error', function(){})
+      it('should throw error', function(){
+        filterer.complete_data = ['test1', 'test2']
+        filterer.filters.test_filter = {
+          fn: function(data, criteria){return (data === criteria.name) ? true : false},
+          criteria: null
+        }
+        expect(filterer.apply_filter('test_filter')).to.throw(
+          Error,
+          'Filters criteria is invalid'
+        )
+      })
     })
     context('when function fails to run', function(){
-      it('should throw error', function(){})
-      it('should show stacktrace in error', function(){})
+      it('should throw error', function(){
+        filterer.complete_data = ['test1', 'test2']
+        filterer.filters.test_filter = {
+          fn: function(data, criteria){return (data === criteria.name) true : false},
+          criteria: {name: 'test1'}
+        }
+        expect(filterer.apply_filter('test_filter')).to.throw(
+          Error,
+          'Unable to process function, please review error message'
+        )
+      })
+      // it('should show stacktrace in error', function(){
+      //   filterer.complete_data = ['test1', 'test2']
+      //   filterer.filters.test_filter = {fn: function(){}, criteria: 'test'}
+      //   expect(filterer.apply_filter('test_filter')).to.throw(
+      //     TypeError,
+      //     'Unable to process function, please review error message'
+      //   )
+      // })
     })
     context('when function filters no data', function(){
-      it('should return null', function(){})
+      it('should return an empty array', function(){
+        filterer.complete_data = ['test1', 'test2']
+        filterer.filters.test_filter = {
+          fn: function(data, criteria){return (data === criteria.name) ? true : false},
+          criteria: {name: 'test3'}
+        }
+        expect(filterer.apply_filter('test_filter')).to.be.empty
+      })
     })
     context('when function filters data successfully', function(){
-      it('should return array', function(){})
-      it('should return array greater than 1 in length', function(){})
-      it('should return an array of the same type as the complete_data array', function(){})
+      it('should return array', function(){
+        filterer.complete_data = ['test1', 'test2']
+        filterer.filters.test_filter = {
+          fn: function(data, criteria){return (data === criteria.name) ? true : false},
+          criteria: {name: 'test1'}
+        }
+        expect(filterer.apply_filter('test_filter'))
+        .to.be.an.instanceof(Array)
+        .that.has.a.lengthOf(1)
+        .and.includes('test1')
+      })
     })
   })
 
   describe('#apply_filters()', function() {
     context('when the complete data array is empty', function(){
-      it('should throw error', function(){})
+      it('should throw error', function(){
+        filterer.complete_data = []
+        filterer.filters.test_filter1 = {
+          fn: function(data, criteria){return (data.name === criteria.name) ? true : false},
+          criteria: {name: 'test1'}
+        }
+        filterer.filters.test_filter2 = {
+          fn: function(data, criteria){return (data.index === criteria.index) ? true : false},
+          criteria: {index: 1}
+        }
+        expect(filterer.apply_filters()).to.throw(
+          Error,
+          'complete_data array empty, provide data to be filtered'
+        )
+      })
     })
     context('when no data is filtered', function(){
-      it('returns null', function(){})
+      it('should return an empty array', function(){
+        filterer.complete_data = [
+          {name: 'test1', index: 0},
+          {name: 'test2', index: 1},
+          {name: 'test3', index: 2}
+        ]
+        filterer.filters.test_filter1 = {
+          fn: function(data, criteria){return (data.name === criteria.name) ? true : false},
+          criteria: {name: 'test4'}
+        }
+        filterer.filters.test_filter2 = {
+          fn: function(data, criteria){return (data.index === criteria.index) ? true : false},
+          criteria: {index: 3}
+        }
+        expect(filterer.apply_filters()).to.be.empty
+      })
     })
     context('when a filter function fails to run', function(){
-      it('should throw an error', function(){})
-      it('should continue to execute other filter functions', function(){})
-      it('should include the name of failed filters in error message', function(){})
+      it('should throw an error', function(){
+        filterer.complete_data = [
+          {name: 'test1', index: 0},
+          {name: 'test2', index: 1},
+          {name: 'test3', index: 2}
+        ]
+        filterer.filters.test_filter1 = {
+          fn: function(data, criteria){return (data.name === criteria.name) ? true : false},
+          criteria: {name: 'test4'}
+        }
+        filterer.filters.test_filter2 = {
+          fn: function(data, criteria){return (data.index === criteria.index) ? true : false},
+          criteria: {index: 3}
+        }
+        expect(filterer.apply_filters()).to.throw(
+          Error,
+          'One or more filters have failed to run'
+        )
+      })
+      // it('should include the name of failed filters in error message', function(){})
     })
     context('when data is filtered', function(){
-      it('should return array', function(){})
-      it('should return array greater than 1 in length', function(){})
-      it('should return an array of the same type as the complete_data array', function(){})
+      it('should return array', function(){
+        filterer.complete_data = [
+          {name: 'test1', index: 0},
+          {name: 'test2', index: 1},
+          {name: 'test3', index: 2}
+        ]
+        filterer.filters.test_filter1 = {
+          fn: function(data, criteria){return (data.name === criteria.name) ? true : false},
+          criteria: {name: 'test3'}
+        }
+        filterer.filters.test_filter2 = {
+          fn: function(data, criteria){return (data.index === criteria.index) ? true : false},
+          criteria: {index: 1}
+        }
+        expect(filterer.apply_filters())
+        .to.be.an.instanceof(Array)
+        .that.has.a.lengthOf(2)
+        .and.includes({name: 'test2', index: 1})
+      })
     })
   })
 })
